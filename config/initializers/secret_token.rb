6adccada3cdc2e78ad56fad3604ddf754ae9399f
Kenneth Devik <kenneth@devik.net>
2010-09-25 23:30:29 +0200

    RoR getting started guide

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogg::Application.config.secret_token = '4271aff8c275d071e4dd26fb9298fc26f3c39f1e7368051e7ea549d5ba710f14260c159f474ba2f871e76031f506673387d929784db148bdb56c1a5a390e919f'
